eb2d11253f0ed14c79cc5cbb50870da276375aa4
Sasank Lakimsetti <saisasank.lakimsetti@gmail.com>
2025-07-21 17:25:32 +0530

    Implemented CustomerController and SavingsAccountController. Tested them in postman

--- src/main/java/com/bms/controller/CustomerController.java
+++ src/main/java/com/bms/controller/CustomerController.java
@@ -1,7 +1,57 @@
 package com.bms.controller;
 
+import com.bms.model.Account;
+import com.bms.model.Customer;
+import com.bms.service.CustomerService;
+import org.apache.coyote.Response;
+import org.apache.jasper.tagplugins.jstl.core.Catch;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+    @PostMapping("/")
+    public ResponseEntity<?>addCustomer(@RequestBody Customer customer){
+        try {
+            customerService.createCustomer(customer);
+            return new ResponseEntity<>(customer, HttpStatus.CREATED);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/customer/{custId}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable Long custId){
+        Customer customer=customerService.getCustomerByCustomerId(custId);
+        if(customer!=null) return new ResponseEntity<>(customer,HttpStatus.FOUND);
+        else return new ResponseEntity<>(customer,HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/update_customer/{customerId}")
+    public ResponseEntity<?>updateCustomer(@PathVariable Long customerId, @RequestBody Customer updatedCustomer){
+        Customer customer=customerService.getCustomerByCustomerId(customerId);
+        if(customer!=null) {
+            customerService.updateCustomer(customerId, updatedCustomer);
+            return new ResponseEntity<>(customer,HttpStatus.OK);
+        }
+        else return new ResponseEntity<>(customer, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/customer/account_details/{customerId}")
+    public ResponseEntity<?> showAccountDetails(@PathVariable Long customerId){
+        Account account=customerService.showAccountDetailsByCustomerId(customerId);
+        if(account!=null) {
+            customerService.showAccountDetailsByCustomerId(customerId);
+            return new ResponseEntity<>(account,HttpStatus.OK);
+        }
+        else return new ResponseEntity<>(account, HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/com/bms/controller/SavingsAccountController.java
+++ src/main/java/com/bms/controller/SavingsAccountController.java
@@ -1,7 +1,32 @@
 package com.bms.controller;
 
+import com.bms.model.Customer;
+import com.bms.model.savings.SavingsAccount;
+import com.bms.repository.savings.SavingsAccountRepository;
+import com.bms.service.CustomerService;
+import com.bms.service.savings.SavingsAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/savings_account")
 public class SavingsAccountController {
+    @Autowired
+    private CustomerService customerService;
+    @Autowired
+    private SavingsAccountRepository savingsAccountRepository;
+    @Autowired
+    private SavingsAccountService savingsAccountService;
+    @ResponseBody
+    @PostMapping("/create_account/{customerId}")
+    public ResponseEntity<?>createSavingsAccount(@PathVariable Long customerId, @RequestBody SavingsAccount account){
+        Customer customer=customerService.getCustomerByCustomerId(customerId);
+        if(customer==null) return new ResponseEntity<>("Customer not found",HttpStatus.NOT_FOUND);
+        account.setCustomerId(customer);
+        savingsAccountRepository.save(account);
+        return new ResponseEntity<>(account, HttpStatus.CREATED);
+    }
 }
