e04d3ad179eee0b5eb8d3eeaf732151d3e468109
Sasank Lakimsetti <saisasank.lakimsetti@gmail.com>
2025-07-21 14:39:46 +0530

    implemented a factory method for handling the form input during registration

---
+++ src/main/java/com/bms/factory/CustomerFactory.java
@@ -0,0 +1,31 @@
+package com.bms.factory;
+
+import com.bms.model.Customer;
+import org.springframework.stereotype.Component;
+
+import java.sql.Date;
+
+@Component
+public class CustomerFactory {
+    public Customer createCustomer(String fname, String lname, Date dob, String address, String mobile, String email, String aadhar, String pan){
+        if(fname.isEmpty()||fname.trim().isEmpty()) throw new RuntimeException("First Name can't be empty.");
+        if(lname.isEmpty()||lname.trim().isEmpty()) throw new RuntimeException("Last Name can't be empty.");
+        if(dob==null) throw new RuntimeException("Date of birth can't be empty.");
+        if(address.isEmpty()||address.trim().isEmpty()) throw new RuntimeException("Address can't be empty.");
+        if((mobile.isEmpty()||mobile.trim().isEmpty())&&(email.isEmpty()||email.trim().isEmpty())) throw  new RuntimeException("Either of the mobile number or mail should be provided.");
+        if (aadhar.isEmpty()||aadhar.trim().isEmpty()) throw new RuntimeException("Aadhar can't be empty.");
+        if(!aadhar.matches("\\d+")) throw new RuntimeException("Aadhar should only have digits.");
+        if(pan.isEmpty()||pan.trim().isEmpty()) throw new RuntimeException("PAN can't be empty.");
+        Customer customer=new Customer();
+        customer.setfName(fname);
+        customer.setLname(lname);
+        customer.setDob(dob);
+        customer.setAddress(address);
+        customer.setMobile(mobile);
+        customer.setMail(email);
+        customer.setAadhar(aadhar);
+        customer.setPan(pan);
+
+        return customer;
+    }
+}
