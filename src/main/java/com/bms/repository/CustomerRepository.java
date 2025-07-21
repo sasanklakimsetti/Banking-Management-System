39ad62087336c84fde71dc64f5380002a715305a
Sasank Lakimsetti <saisasank.lakimsetti@gmail.com>
2025-07-21 14:38:38 +0530

    Implemented repositories for Account and Customer classes

--- src/main/java/com/bms/repository/CustomerRepository.java
+++ src/main/java/com/bms/repository/CustomerRepository.java
@@ -4,6 +4,19 @@ import com.bms.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByMobile(String mobile);
+
+    Customer findByAadhar(String aadhar);
+
+    Customer findByFName(String fname);
+
+    Customer findByLName(String lname);
+
+    Customer findByPan(String pan);
+
+    List<Customer> findByAddress(String address);
 }
