126a4088d9dd475a9451179fb498952e06bfdd2c
Sasank Lakimsetti <saisasank.lakimsetti@gmail.com>
2025-07-22 14:39:18 +0530

    Recovered and updated files after switching back

--- src/main/java/com/bms/repository/savings/SavingsAccountRepository.java
+++ src/main/java/com/bms/repository/savings/SavingsAccountRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SavingsAccountRepository extends JpaRepository<SavingsAccount, Long> {
     SavingsAccount findByCustomerId_CustomerId(Long customerId);
     List<SavingsAccount> findAllByCustomerId_CustomerId(Long customerId);
+
+    Optional<SavingsAccount> findByAccountNumber(Long accountNumber);
 }
